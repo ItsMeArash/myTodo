3db0baa8312be41532ea77675bb9fdeb3c46a9a2
ItsMeArash <arashh7998@gmail.com>
2022-12-18 17:20:06 +0330

    submitting todo added

---
+++ script.js
@@ -0,0 +1,32 @@
+const todoInput = document.querySelector('.todo-input');
+const todoButton = document.querySelector('.todo-button');
+const todoList = document.querySelector('.todo-list');
+
+todoButton.addEventListener('click', addTodo);
+
+function addTodo(event)
+{
+    event.preventDefault();
+
+    const todoDiv = document.createElement('div');
+    todoDiv.classList.add('todo');
+
+    const newTodo = document.createElement('li');
+    newTodo.innerText = todoInput.value;
+
+    newTodo.classList.add('todo-item');
+    todoDiv.appendChild(newTodo);
+    todoInput.value = "";
+
+    const completeButton = document.createElement('button');
+    completeButton.classList.add('complete-btn');
+    completeButton.innerHTML = '<i class="fas fa-check"></i>';
+    todoDiv.appendChild(completeButton);
+
+    const trashButton = document.createElement('button');
+    trashButton.classList.add('trash-btn');
+    trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+    todoDiv.appendChild(trashButton);
+
+    todoList.appendChild(todoDiv);
+}
